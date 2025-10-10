e1608ed26d5f0a3bd1c14bf5b1d35669ccd2aa83
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-10-10 15:27:15 -0400

    Constructor,getters and setters for Transaction

---
+++ src/main/java/com/Pluralsight/Transaction.java
@@ -0,0 +1,50 @@
+package com.Pluralsight;
+
+import java.time.LocalDate;
+
+public class Transaction {
+
+    private LocalDate dateTime;
+    private String vendor;
+    private String description;
+    private double amount;
+
+    public Transaction(LocalDate dateTime, String vendor, String description, double amount) {
+        this.dateTime = dateTime;
+        this.vendor = vendor;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public LocalDate getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDate dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
