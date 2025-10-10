8c6d9742517c3fb35e5322ea9319cdaa95312401
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-10-10 15:08:01 -0400

    while for homescreen

--- src/main/java/com/Pluralsight/AccountingLedgerApplication.java
+++ src/main/java/com/Pluralsight/AccountingLedgerApplication.java
@@ -1,7 +1,24 @@
 package com.Pluralsight;
 
+import java.util.Scanner;
+
 public class AccountingLedgerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Scanner scanner= new Scanner(System.in);
+
+        boolean homeScreen=true;
+
+        while (homeScreen) {
+            System.out.println("\n===== HOME SCREEN =====");
+            System.out.println("Choose an option:");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+            System.out.print("Enter choice: ");
+
+            String choice = scanner.nextLine().trim().toUpperCase();
+        }
     }
 }
